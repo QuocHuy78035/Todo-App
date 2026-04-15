e69007d2e8cc185ab4702337b74ee2b196ce8131
QuocHuy78035 <huypro0320092003@gmail.com>
2026-04-15 13:40:48 +0700

    feat: add story-book

---
+++ src/stories/Buttons/button.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from "@storybook/react-vite";
+import { Button } from "~/components/Buttons";
+
+const meta: Meta<typeof Button> = {
+  title: "Components/ButtonComponents/Button",
+  component: Button,
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      control: "radio",
+      options: ["small", "medium", "large"]
+    },
+    variant: {
+      control: "select",
+      options: ["primary", "secondary", "tertiary", "destructive"]
+    },
+    disabled: {
+      control: "boolean"
+    },
+    children: {
+      control: "text"
+    }
+  }
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Playground: Story = {
+  args: {
+    children: "New template",
+    size: "medium",
+    variant: "primary",
+    disabled: false
+  }
+};
+
+export const Showcase: Story = {
+  render: () => {
+    return <Button>New template</Button>;
+  }
+};
