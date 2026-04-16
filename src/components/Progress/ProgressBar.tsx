3a087275a976003f05db58909ed20caa2517181f
QuocHuy78035 <huypro0320092003@gmail.com>
2026-04-16 15:29:49 +0700

    style: updated progress bar component

--- src/components/Progress/ProgressBar.tsx
+++ src/components/Progress/ProgressBar.tsx
@@ -1,22 +1,31 @@
 import { cn } from "~/utils";
 
-type ProgressBarBgColor = "bgInProgress" | "bgExpired" | "bgDone";
-
 interface ProgressBarProps {
-  bgColor: ProgressBarBgColor;
+  isDone?: boolean;
+  isExpired?: boolean;
   progress: number;
   isHavePercent?: boolean;
 }
 
-export const ProgressBar = ({ bgColor, progress, isHavePercent = false }: ProgressBarProps) => {
-  const bgClasses = bgColor ? `bg-${bgColor}` : "";
+export const ProgressBar = ({
+  isDone = false,
+  isExpired = false,
+  progress,
+  isHavePercent = false
+}: ProgressBarProps) => {
+  const getBgColor = (isDone: boolean, isExpired: boolean) => {
+    if (isDone) return "bg-bgDone";
+    if (isExpired) return "bg-bgExpired";
+    return "bg-bgInProgress";
+  };
+
   return (
     <div className={cn("h-1 w-full rounded-[40px] bg-borderSecondary relative")}>
       <div
         style={{
           width: `${progress}%`
         }}
-        className={cn(bgClasses, `h-1 rounded-[40px] relative flex justify-center`)}
+        className={cn(getBgColor(isDone, isExpired), `h-1 rounded-[40px] relative flex justify-center`)}
       >
         {isHavePercent && <span className="absolute text-[4px] text-white">{`${progress}%`}</span>}
       </div>
